4fe31f2a739b8096e7be2959e71514d57a6a1c8f
Muhammad Azri Fatihah Susanto <90236860+muhAzri@users.noreply.github.com>
2022-01-09 08:43:17 +0700

    Update settings.py

--- admirer/settings.py
+++ admirer/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&co==8(#p7h+z_(g(w+isw+&=rem!@_)673&7aye+(t)9m&xt#'
+SECRET_KEY = #Secret KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
